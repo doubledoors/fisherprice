5c358da0cc5e4247587651d5d7dcfe79e63b65ad
Ben Sharp <bfwsharp@gmail.com>
2016-09-23 15:49:09 +0100

    test wincred

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Input a URL</h2>
+          <h2>Input a youtube URL</h2>
         </div>
         <div>
           <UserInput/>

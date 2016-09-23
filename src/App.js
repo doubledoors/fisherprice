6020bad0ef17b03521afca1363fd8cda5c6f4637
Ben Sharp <bfwsharp@gmail.com>
2016-09-23 15:39:04 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import UserInput from './components/UserInput.js';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Input a URL</h2>
+        </div>
+        <div>
+          <UserInput/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/UserInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class UserInput extends Component {
+  render() {
+    return(
+      <div>
+        <input type="text" name="userinput"/>
+      </div>
+    );
+  }
+}
